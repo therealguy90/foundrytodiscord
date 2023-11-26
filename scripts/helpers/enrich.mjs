1a16d5a55fced8a341769fa3419fdbf3d199981d
Jmalayao <jmalayao43@gmail.com>
2023-11-26 19:55:14 +0800

    add helper for enrichHTML

---
+++ scripts/helpers/enrich.mjs
@@ -0,0 +1,9 @@
+export async function newEnrichedMessage(message, options = {}){
+    const enrichedMessage = message;
+    enrichedMessage.content = await toHTML(enrichedMessage.content, options);
+    return enrichedMessage;
+}
+
+export async function toHTML(content, options ={}){
+    return await TextEditor.enrichHTML(content, options);
+}
