1fedaafe4c7bb7ed654d9cf6cf774c3e9562bff1
Jmalayao <jmalayao43@gmail.com>
2024-02-15 18:27:05 +0800

    fix capital action glyphs not turning into emojis

--- scripts/pf2e.mjs
+++ scripts/pf2e.mjs
@@ -85,7 +85,7 @@ async function PF2e_parseHTMLText(htmlString) {
     // Format various elements
     generic.removeElementsBySelector('[data-visibility="gm"], [data-visibility="owner"],[data-visibility="none"]', htmldoc);
     generic.formatTextBySelector('.inline-check, span[data-pf2-check]', text => `${dieIcon(20)}\`${text}\``, htmldoc);
-    generic.formatTextBySelector('.action-glyph', text => `${text.replace(/1|2|3|f|r/g, match => actionGlyphEmojis[match])}`, htmldoc);
+    generic.formatTextBySelector('.action-glyph', text => `${text.replace(/1|2|3|4|5|a|d|t|f|r/g, match => swapOrNot(match.toLowerCase(), actionGlyphEmojis[match.toLowerCase()]))}`, htmldoc);
     generic.formatTextBySelector('.statements.reverted', text => `~~${text}~~`, htmldoc);
     reformattedText = htmldoc.innerHTML;
     htmldoc.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(header => {
