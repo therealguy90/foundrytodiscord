f4f9cbc6330e8ff42f66c661af25e73e66d1bb47
Jmalayao <jmalayao43@gmail.com>
2024-05-26 22:41:10 +0800

    remove deprecation warning for Color

--- scripts/helpers/parser/messages.mjs
+++ scripts/helpers/parser/messages.mjs
@@ -233,6 +233,7 @@ export async function addEmbedsToRequests(allRequests, hook, username, imgurl, e
 }
 
 export function hexToColor(hex) {
+    hex = hex.css || hex; /*Will be removed in v13*/
     // Remove the '#' character if it exists at the beginning of the hex string
     hex = hex.replace(/^#/, '');
 
