e583b32936c525c35eeed1f7e34f543deef87ca8
Jmalayao <jmalayao43@gmail.com>
2025-09-21 21:21:38 +0800

    remove log

--- scripts/systemparsers/pf1.mjs
+++ scripts/systemparsers/pf1.mjs
@@ -49,7 +49,6 @@ export class MessageParserPF1 extends MessageParser {
             //Attack Roll
             const attackTitleElement = attackElement.querySelector(`.attack-flavor.alt, .attack-flavor[colspan="2"]`);
             const attack = message.systemRolls.attacks[rollIndex];
-            console.log(attack.attack);
             const attackRoll = attack.attack;
             if (attackRoll) {
                 let attackFieldValue = `\`${attackRoll.formula}\`\n${dieIcon(20)}**\`${attackRoll.total}\`**||(${this._generateRollBreakdown(attackRoll)})||\n`
@@ -147,32 +146,4 @@ export class MessageParserPF1 extends MessageParser {
         return (doc.querySelector('.level-up') !== null);
     }
 
-
-    /* These methods can be overriden. Refer to other parsers as to how this is done.
-    * 
-    * _systemHTMLParser is used to parse custom css tags and text that your system uses.
-    * 
-    * _systemHTMLParser(htmlString) {
-    *     return htmlString;
-    * }
-    * 
-    * _getSystemSpecificCards is used for system- or module-specific chat cards that need special parsing.
-    * Note that "message" is already enriched.
-    * This will return an array of embeds. Refer to the Discord Webhook docs.
-    * async _getSystemSpecificCards(message) {
-    *     return [];
-    * }
-    * 
-    * 
-    * _getEnrichmentOptions is used for enriching system-specific text or links. Check your system's enricher.
-    * This should return an Object containing the enrichment options.
-    * _getEnrichmentOptions(message){
-    *     return {};
-    * }
-    * 
-    * 
-    * 
-    * 
-    * 
-    */
 }
